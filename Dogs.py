adc6f6783719ef4868adcf7865f7742e6686a450
Proba <teplyakovateplyakova94@gmail.com>
2024-10-20 00:11:57 +1000

    Устранен баг с пустыми значениями спинбоксов. Добавлены значения по умолчанию 300

--- Dogs.py
+++ Dogs.py
@@ -64,11 +64,13 @@ width_label = ttk.Label(text="Ширина")
 width_label.pack(side="left", padx=(10,0))
 width_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
 width_spinbox.pack(side="left", padx=(0, 10))
+width_spinbox.set(300)
 
 height_label = ttk.Label(text="Высота")
 height_label.pack(side="left", padx=(10,0))
 height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
 height_spinbox.pack(side="left", padx=(0, 10))
+height_spinbox.set(300)
 
 top_level_window = Toplevel(window)
 top_level_window.title("Изображения собачек")
